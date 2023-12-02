f6ed268881590259bffae27f783ffff4f2bfc79d
Rares <raresficiu1@gmail.com>
2023-12-02 00:31:34 +0000

    added server files

---
+++ flask-server/server.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_cors import CORS
+
+
+app = Flask(__name__)
+
+#Members API Route
+
+@app.route("/members")
+def members():
+    return{"members":['Member1','member2']}
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    CORS(app)
