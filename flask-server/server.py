97cb3b7eb60d3f3aeca79b30160e73fac71e465b
Rares F <36771504+raresficiu1@users.noreply.github.com>
2023-12-17 00:26:23 +0000

    test

--- flask-server/server.py
+++ flask-server/server.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 #Members API Route
 @app.route("/members")
 def members():
-    return{"members":['Member1','member2']}
+    return{"members":['MEMBER6969696','member2']}
 
 #jk tst
 if __name__ == "__main__":
